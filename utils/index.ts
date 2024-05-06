2bdabc8c621ec86351f0c6fae2552e5d64bc2ddb
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-05-06 21:56:07 +0200

    Add fermat test

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,6 @@
 /**
   Return the square of a number
   @param x number to square
-  @returns square of x
  */
 export function square(x: number): number {
   return x * x
@@ -10,16 +9,14 @@ export function square(x: number): number {
 /**
   Returns the cube of a number
   @param x number to cube
-  @returns cube of x
  */
 export function cubed(x: number): number {
   return x * x * x
 }
 
 /**
-  Returns the absolute value of a number
-  @param x number to get the absolute value of
-  @returns absolute value of x
+  Returns the absolute value of a number (the value without regard to whether it is positive or negative). For example, the absolute value of -5 is the same as the absolute value of 5.
+  @param x A numeric expression for which the absolute value is needed.
  */
 export function abs(x: number): number {
   return Math.abs(x)
@@ -28,7 +25,6 @@ export function abs(x: number): number {
 /**
   Checks if a number is even
   @param n number to check
-  @returns true if even, false if odd
  */
 export function is_even(n: number): boolean {
   return n % 2 === 0
@@ -37,7 +33,6 @@ export function is_even(n: number): boolean {
 /**
   Double a number
   @param x number to double
-  @returns doubled number
  */
 export function double(x: number): number {
   return x + x
@@ -46,8 +41,22 @@ export function double(x: number): number {
 /**
   Halves a number
   @param x number to halve
-  @returns halved number
  */
 export function halve(x: number): number {
   return x / 2
+}
+
+/**
+  Returns the greatest integer less than or equal to its numeric argument.
+  @param x — A numeric expression.
+ */
+export function math_floor(x: number): number {
+  return Math.floor(x)
+}
+
+/**
+  Returns a pseudorandom number between 0 and 1.
+ */
+export function math_random(): number {
+  return Math.random()
 }
