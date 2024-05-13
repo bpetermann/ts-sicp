44bf982b65bbf40f7eb4043c0d28669e0218dbb9
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-05-13 22:28:48 +0200

    Add exercise 1.44

--- src/1/1.3/1.44.test.ts
+++ src/1/1.3/1.44.test.ts
@@ -4,25 +4,22 @@ import { cube } from '../../../utils'
 test('1.42', () => {
   type UnaryFunction = (x: number) => number
 
-  function compose<T extends UnaryFunction>(f: T, g: T): (x: number) => ReturnType<T> {
-    return (x: number) => f(g(x)) as ReturnType<T>
+  function compose<T extends UnaryFunction>(f: T, g: T): UnaryFunction {
+    return (x: number) => f(g(x))
   }
 
-  function repeated<T extends (n: any) => any>(
-    f: T,
-    n: number
-  ): ((x: number | UnaryFunction) => any) | ((x: any) => ReturnType<T>) {
-    return n === 0 ? (x: number | UnaryFunction) => x : compose(f, repeated(f, n - 1))
+  function repeated<T extends UnaryFunction>(f: T, n: number): UnaryFunction {
+    return n === 0 ? (x: number) => x : compose(f, repeated(f, n - 1))
   }
 
-  function smooth<T extends UnaryFunction>(f: T): UnaryFunction {
+  function smooth(f: UnaryFunction): UnaryFunction {
     const dx = 0.00001
-    return (x) => (f(x - dx) + f(x) + f(x + dx)) / 3
+    return (x: number) => (f(x - dx) + f(x) + f(x + dx)) / 3
   }
 
   function n_fold_smooth(f: UnaryFunction, n: number) {
-    return repeated(smooth, n)(f)
+    return repeated(smooth(f), n)
   }
 
-  expect(n_fold_smooth(cube, 5)(4)).toBeCloseTo(64)
+  console.log(n_fold_smooth(cube, 5)(4)) // Output: 64
 })
