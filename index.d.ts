77cfe6871c8abea8a9bf78233936afc7f5287e70
bpetermann <benjamin.petermann@gmx.at>
2024-05-17 17:26:14 +0200

    Add sicp types

--- index.d.ts
+++ index.d.ts
@@ -12,28 +12,6 @@ declare module 'sicp' {
   export function display<T>(v: T, s?: string): T
 
   /**
-   * Makes a pair whose head (first component) is x
-   * and whose tail (second component) is y.
-   *
-   * Time: Theta(1) Theta(1)
-   *
-   * @param {T} x Value for the head.
-   * @param {T} y Value for the tail.
-   * @returns {[T, T]} A pair with x as the head and y as the tail.
-   */
-  export function pair<T>(x: T, y: T): [T, T]
-
-  /**
-   * Returns head (first component) of given pair p.
-   *
-   * Time: Theta(1) Theta(1)
-   *
-   * @param {Pair<T>} p given pair
-   * @returns {T} head of p
-   */
-  export function head<T>(p: [T, T]): T
-
-  /**
    * Returns a string that represents the value v,
    * using a notation that is is consistent with JSON,
    * but also displays undefined, NaN, Infinity, and function objects.
@@ -44,12 +22,74 @@ declare module 'sicp' {
   export function stringify(v: unknown): string
 
   /**
+   * Given zero or more numbers, returns the largest of them.
+   * If no arguments are given, the result is -∞.
+   * If any value is NaN, the result is NaN.
+   * The comparison of values to determine the largest value is done
+   * using the Abstract Relational Comparison algorithm except that
+   * +0 is considered to be larger than -0.
+   *
+   * @param {number[]} values given numbers
+   * @returns {number}  largest of them
+   *
+   */
+  export function math_max(...values: number[]): number
+
+  /**
+   * Given zero or more arguments, returns the smallest of them.
+   * If no arguments are given, the result is +∞.
+   * If any value is NaN, the result is NaN.
+   * The comparison of values to determine the smallest value is done
+   * using the Abstract Relational Comparison algorithm except that
+   * +0 is considered to be larger than -0.
+   *
+   * @param {number[]} values given numbers
+   * @returns {number}  largest of them
+   *
+   */
+  export function math_min(...values: number[]): number
+
+  /**
+   * Makes a pair whose head (first component) is x
+   * and whose tail (second component) is y.
+   *
+   * Time: Theta(1) Theta(1)
+   *
+   * @param {T} x Value for the head.
+   * @param {U} y Value for the tail.
+   * @returns {[T, U]} A pair with x as the head and y as the tail.
+   */
+  export function pair<T, U>(x: T, y: U): [T, U]
+
+  /**
+   * Returns head (first component) of given pair p.
+   *
+   * Time: Theta(1) Theta(1)
+   *
+   * @param {Pair<T, U>} p given pair
+   * @returns {T} head of p
+   */
+  export function head<T, U>(p: [T, U]): T
+
+  /**
    * Returns tail (second component) of given pair p.
    *
    * Time: Theta(1) Theta(1)
    *
-   * @param {Pair<T>} p given pair
-   * @returns {T} head of p
+   * @param {Pair<T, U>} p given pair
+   * @returns {U} tail of p
    */
-  export function tail<T>(p: [T, T]): T
+  export function tail<T, U>(p: [T, U]): U
+
+  /**
+   * **primitive**; given n values, returns a list of length n.
+   * The elements of the list are the given values in the given order
+   *
+   * Time: Theta(1) Theta(1)
+   *
+   * @param {unknown[]} args - The given values.
+   * @returns {NestedPairs<T>} A list containing all values in the given order.
+   */
+  type NestedPairs<T> = T extends [arg: infer U, ...rest: infer Rest] ? [U, NestedPairs<Rest>] : null
+  export function list<T extends unknown[]>(...args: T): NestedPairs<T>
 }
--- src/2/2.1/2.4.test.ts
+++ src/2/2.1/2.4.test.ts
@@ -1,18 +1,16 @@
-type Pair = (m: (p: number, q: number) => number) => number
-
-function pair(x: number, y: number): Pair {
-  return (m) => m(x, y)
-}
-
-function head(z: Pair): number {
-  return z((p) => p)
-}
-
-function tail(z: Pair): number {
-  return z((_, q) => q)
-}
-
 test('2.4', () => {
+  function pair(x: number, y: number): (m: (p: number, q: number) => number) => number {
+    return (m) => m(x, y)
+  }
+
+  function head(z: (m: (p: number, q: number) => number) => number): number {
+    return z((p) => p)
+  }
+
+  function tail(z: (m: (p: number, q: number) => number) => number): number {
+    return z((_, q) => q)
+  }
+
   const x = pair(1, 2)
   expect(head(x)).toBe(1)
   expect(tail(x)).toBe(2)
