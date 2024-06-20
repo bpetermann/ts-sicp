ab3f4d65105f93422b37e67b0522aa8d2c1e976f
bpetermann <benjamin.petermann@gmx.at>
2024-06-20 21:29:51 +0200

    Update bookmark script

--- utils/bookmark.ts
+++ utils/bookmark.ts
@@ -3,16 +3,13 @@ import colors from 'colors'
 import simpleGit from 'simple-git'
 
 const BASE_BOOK = 'https://sourceacademy.org/sicpjs/' as const
-const README = './README.md' as const
+const README_PATH = './README.md' as const
 
-const commit = async (bookmark: string) => {
-  try {
-    await simpleGit().add(README).commit(`Update bookmark to ${bookmark} 🔖`).push('origin', 'main')
-    console.log('Changes committed and pushed successfully')
-  } catch (error) {
-    console.error('Error committing changes:', error)
-  }
-}
+const success = (text: string) => console.log(`${colors.green(text)}`)
+const error = (text: string) => console.error(`${colors.red(text)}`)
+
+const commit = async (bookmark: string) =>
+  await simpleGit().add(README_PATH).commit(`Update bookmark to ${bookmark} 🔖`).push('origin', 'main')
 
 const updateText = (data: string, bookmark: string) =>
   data
@@ -22,37 +19,36 @@ const updateText = (data: string, bookmark: string) =>
 
 const update = async (bookmark: string) => {
   try {
-    const data = await fs.readFile(README, { encoding: 'utf8' })
+    const data = await fs.readFile(README_PATH, { encoding: 'utf8' })
     const updatedText = updateText(data, bookmark)
-    await fs.writeFile(README, updatedText, { encoding: 'utf8' })
-    console.log('README.md updated successfully')
-    await commit(bookmark) // Ensure commit is awaited
-  } catch (error) {
-    console.error('Error updating README.md:', error)
+    await fs.writeFile(README_PATH, updatedText, { encoding: 'utf8' })
+    success('README.md updated successfully')
+    await commit(bookmark)
+    success('Changes committed and pushed successfully')
+  } catch (e) {
+    error(e)
   }
 }
 
 const bookmarkText = (data: string) => {
   const line = data.split('\n').find((line) => line.startsWith('➜ Bookmark'))
-  return line ? line.split('➜ Bookmark: ')[1] : null // Return null if no bookmark is found
+  return line ? line.split('➜ Bookmark: ')[1] : null
 }
 
-const logBookmark = async () => {
+const getBookmark = async () => {
   try {
-    const data = await fs.readFile(README, { encoding: 'utf8' })
+    const data = await fs.readFile(README_PATH, { encoding: 'utf8' })
     const bookmark = bookmarkText(data)
     if (bookmark) {
       console.log(`${colors.green('➜  ')}${colors.white('Index:    ')}${colors.cyan(BASE_BOOK)}`)
       console.log(`${colors.green('➜  ')}${colors.white('Bookmark: ')}${colors.cyan(`${bookmark}`)}`)
-    } else {
-      console.log(`${colors.red('No bookmark found in README.md')}`)
-    }
-  } catch (error) {
-    console.error('Error reading README.md:', error)
+    } else error('No bookmark found in README.md')
+  } catch (e) {
+    error(e)
   }
 }
 
 ;(async function () {
   if (process.argv[2]) await update(process.argv[2])
-  else await logBookmark()
+  else await getBookmark()
 })()
